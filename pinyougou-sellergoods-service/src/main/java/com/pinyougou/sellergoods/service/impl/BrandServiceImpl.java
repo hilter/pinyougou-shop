b0a0363ff8021562ac3a799a8ec77503af058ede
AmazeLee <911711054@qq.com>
2018-07-07 13:16:48 +0800

    品牌管理完成

--- pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -2,15 +2,18 @@ package com.pinyougou.sellergoods.service.impl;
 
 import java.util.List;
 
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-import com.pinyougou.entity.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.pinyougou.mapper.TbBrandMapper;
 import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.pojo.TbBrandExample;
+import com.pinyougou.pojo.TbBrandExample.Criteria;
 import com.pinyougou.sellergoods.service.BrandService;
+
+import entity.PageResult;
 @Service
 public class BrandServiceImpl implements BrandService {
 
@@ -23,16 +26,60 @@ public class BrandServiceImpl implements BrandService {
 		return brandMapper.selectByExample(null);
 	}
 
-	/**
-	 * 品牌分页
-	 * @param pageNum 当前页面
-	 * @param pageSize  每页记录数
-	 * @return
-	 */
+	@Override
 	public PageResult findPage(int pageNum, int pageSize) {
-		// 分页
-		PageHelper.startPage(pageNum, pageSize);
+		
+		PageHelper.startPage(pageNum, pageSize);//分页
+		
 		Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(null);
+		
 		return new PageResult(page.getTotal(), page.getResult());
 	}
+
+	@Override
+	public void add(TbBrand brand) {
+		
+		brandMapper.insert(brand);
+	}
+
+	@Override
+	public TbBrand findOne(Long id) {
+		
+		return brandMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public void update(TbBrand brand) {
+		brandMapper.updateByPrimaryKey(brand);
+	}
+
+	@Override
+	public void delete(Long[] ids) {				
+		for(Long id:ids){
+			brandMapper.deleteByPrimaryKey(id);
+		}		
+	}
+
+	@Override
+	public PageResult findPage(TbBrand brand, int pageNum, int pageSize) {
+		
+		PageHelper.startPage(pageNum, pageSize);//分页	
+		
+		TbBrandExample example=new TbBrandExample();
+		
+		Criteria criteria = example.createCriteria();
+		if(brand!=null){
+			if(brand.getName()!=null && brand.getName().length()>0){
+				criteria.andNameLike("%"+brand.getName()+"%");
+			}
+			if(brand.getFirstChar()!=null && brand.getFirstChar().length()>0){
+				criteria.andFirstCharLike("%"+brand.getFirstChar()+"%");
+			}			
+		}
+		
+		Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(example);
+		
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
 }
