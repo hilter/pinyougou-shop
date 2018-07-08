c4cc73378466ac68e87530861ebcafdbecbc6c96
AmazeLee <911711054@qq.com>
2018-07-08 22:38:43 +0800

    AngularJs控制器继承

--- pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
+++ pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
@@ -1,4 +1,7 @@
-app.controller('brandController',function($scope,$http,brandService){
+app.controller('brandController',function($scope,$http,$controller,brandService){
+
+    $controller('baseController',{$scope:$scope});
+
 
     //查询品牌列表
     $scope.findAll=function(){
@@ -7,23 +10,6 @@ app.controller('brandController',function($scope,$http,brandService){
                 $scope.list=response;
             }
         );
-    }
-
-
-    //分页控件配置currentPage:当前页   totalItems :总记录数  itemsPerPage:每页记录数  perPageOptions :分页选项  onChange:当页码变更后自动触发的方法
-    $scope.paginationConf = {
-        currentPage: 1,
-        totalItems: 10,
-        itemsPerPage: 10,
-        perPageOptions: [10, 20, 30, 40, 50],
-        onChange: function(){
-            $scope.reloadList();
-        }
-    };
-
-    //刷新列表
-    $scope.reloadList=function(){
-        $scope.search( $scope.paginationConf.currentPage ,  $scope.paginationConf.itemsPerPage );
     }
 
     //分页
@@ -64,16 +50,7 @@ app.controller('brandController',function($scope,$http,brandService){
         );
     }
 
-    $scope.selectIds=[];//用户勾选的ID集合
-    //用户勾选复选框
-    $scope.updateSelection=function($event,id){
-        if($event.target.checked){
-            $scope.selectIds.push(id);//push向集合添加元素
-        }else{
-            var index= $scope.selectIds.indexOf(id);//查找值的 位置
-            $scope.selectIds.splice(index,1);//参数1：移除的位置 参数2：移除的个数
-        }
-    }
+
 
     //删除
     $scope.dele=function(){
